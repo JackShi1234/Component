315b40f9038746c1fbb7140d6daa8dd10aadfb5f
张小二 <yuantuo@zhangxiaoer.local>
2020-12-07 11:50:07 +0800

    初始化

---
+++ demo/demo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by 张小二 on 2020/12/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demo/demo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  demo
+//
+//  Created by 张小二 on 2020/12/7.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
